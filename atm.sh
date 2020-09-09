2c786775cae20e58906945a8e552957b3679b46f
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-09-09 19:38:51 +0100

    Display dashed lines

--- atm.sh
+++ atm.sh
@@ -4,18 +4,26 @@
 PIN="0"
 TTYPE="0"
 
+function displayDashedLine {
+    echo "---------------------------------------------"
+}
+
 function getPin {
+    displayDashedLine
     echo "Welcome, please enter your pin at the prompt:"
+    displayDashedLine
     read PIN
 }
 
 function getTransactionType {
+    displayDashedLine
     echo "Please select transaction type:"
-    echo ""
+    displayDashedLine
     echo "Select 1 to make a Withdrawal."
     echo "Select 2 to make a Transfer."
     echo "Select 3 to Change your PIN."
     echo "Select 4 to Buy Airtime"
+    displayDashedLine
     read TTYPE
 }
 
