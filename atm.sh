22b8428fe1787321eeaf01b3c43e2897f8db8662
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-09-09 20:02:15 +0100

    Read amount index and infer amount from it

--- atm.sh
+++ atm.sh
@@ -2,8 +2,10 @@
 
 
 PIN="0"
-TRANTYPE="0"
-ACCTYPE="0"
+TRANSACTION_TYPE="0"
+ACCOUNT_TYPE="0"
+AMOUNT_INDEX="0"
+AMOUNT="0"
 
 function displayDashedLine {
     echo "---------------------------------------------"
@@ -13,6 +15,7 @@ function getPin {
     displayDashedLine
     echo "Welcome, please enter your pin at the prompt:"
     displayDashedLine
+
     read PIN
 }
 
@@ -25,7 +28,8 @@ function getTransactionType {
     echo "Select 3 to Change your PIN."
     echo "Select 4 to Buy Airtime"
     displayDashedLine
-    read TRANTYPE
+
+    read TRANSACTION_TYPE
 }
 
 function getAccountType {
@@ -35,12 +39,56 @@ function getAccountType {
     echo "Select 1 for Current"
     echo "Select 2 for Savings"
     displayDashedLine
-    read ACCTYPE
+
+    read ACCOUNT_TYPE
 }
+
+function getAmountIndex {
+
+    displayDashedLine
+    echo "Please select amount"
+    displayDashedLine
+    echo "Select 1 for 5,000"
+    echo "Select 2 for 10,000"
+    echo "Select 3 for 15,000"
+    echo "Select 4 for 20,000"
+    echo "Select 5 to enter a custom amount"
+
+    read AMOUNT_INDEX
+
+    getAmount
+}
+
+function getAmount {
+    if [[ $AMOUNT_INDEX -eq "1" ]]
+    then
+        AMOUNT="5000"
+    elif [[ $AMOUNT_INDEX -eq "2" ]]
+    then
+        AMOUNT="10000"
+    elif [[ $AMOUNT_INDEX -eq "3" ]]
+    then
+        AMOUNT="15000"
+    elif [[ $AMOUNT_INDEX -eq "4" ]]
+    then
+        AMOUNT="20000"
+    else
+        AMOUNT="Custom Amount"
+    fi
+
+}
+
+# function payAmount {
+#     echo "Please take your cash -- $AMOUNT"
+# }
 
 getPin
 echo $PIN
 getTransactionType
-echo $TRANTYPE
+echo $TRANSACTION_TYPE
 getAccountType
-echo $ACCTYPE
+echo $ACCOUNT_TYPE
+getAmountIndex
+echo $AMOUNT_INDEX
+getAmount
+echo $AMOUNT
